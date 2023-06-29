7279094e200912d6018994750ccd3548d96d2973
Ândriu <andriu_coelho1993@hotmail.com>
2023-06-29 18:46:19 -0300

    aula 3

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -8,20 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Attributes
+    
+    @StateObject private var service = HomeService()
+    @State private var storeTypes: [StoreType] = []
+    
+    @State private var isLoading = true
+    
+    // MARK: - View
+    
     var body: some View {
         NavigationView {
             VStack {
-                NavigationBar()
-                    .padding(.horizontal, 15)
-                
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: 20) {
-                        OrderTypeGridView()
-                        CarouselTabView()
-                        StoresContainerView()
+                if isLoading {
+                    ProgressView()
+                } else {
+                    NavigationBar()
+                        .padding(.horizontal, 15)
+                    
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack(spacing: 20) {
+                            OrderTypeGridView()
+                            CarouselTabView()
+                            StoresContainerView(stores: storeTypes)
+                        }
                     }
                 }
             }
+        }
+        .onAppear {
+            Task {
+                await getStores()
+            }
+        }
+    }
+    
+    func getStores() async {
+        do {
+            let result = try await service.fetchData()
+            switch result {
+            case .success(let stores):
+                self.storeTypes = stores
+                self.isLoading = false
+            case .failure(let error):
+                print(error.localizedDescription)
+                self.isLoading = false
+                break
+            }
+        } catch {
+            print(error.localizedDescription)
+            self.isLoading = false
         }
     }
 }
